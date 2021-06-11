b45459667030e6774961026e446d235b345f3802
Beerus-Kun <67775920+Beerus-Kun@users.noreply.github.com>
2021-06-11 23:26:36 +0700

    them 1 so thu lien quan den fashion

--- src/routes/fashion.js
+++ src/routes/fashion.js
@@ -1,11 +1,11 @@
 var express = require('express');
 const router = express.Router();
+const fashionController = require('../app/controllers/fashionController')
 
-const siteController = require('../app/controllers/fashionController');
 
-router.get('fashion/style-for-couple', fashionController.fashionStyleOfMe);
-router.get('fashion/style-of-me', fashionController.fashionStyleForCouple);
-router.get('fashion/wash-jean', fashionController.fashionWashJean);
-router.get('/', fashionController.index);
+router.get('/style-for-couple', fashionController.fashionStyleOfMe);
+router.get('/style-of-me', fashionController.fashionStyleForCouple);
+router.get('/wash-jean', fashionController.fashionWashJean);
+// router.get('/', fashionController.index);
 
 module.exports = router;
